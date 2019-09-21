2c2d2690e076cb1e34d76d5c6b5941146f5c80af
camerrg <52823370+camerrg@users.noreply.github.com>
2019-09-21 09:00:09 -0500

    Test Assignment
    I could get the test to work properly. I tried to figure it out. Of course, I didn't get all the points necessary.  I will will send over the original program as well for the test.

---
+++ OnlineBookCameron2Test.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OnlineBookCameron2Test {
+
+	@Test
+	void testdeterminePrice() {
+		assertEquals(79.71, 79.71, 0.01);
+		
+	}
+	
+	@Test
+	void testdeterminePrice() {
+		assertEquals(144.27,144.27, 0.01);
+	}
+	
+
+}
